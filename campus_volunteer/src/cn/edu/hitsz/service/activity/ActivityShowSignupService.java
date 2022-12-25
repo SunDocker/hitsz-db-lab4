895382adf3075c586808bc904f1d35e4e0f24f57
Sun Duo <1075079562@qq.com>
2022-12-25 15:27:57 +0800

    update

---
+++ campus_volunteer/src/cn/edu/hitsz/service/activity/ActivityShowSignupService.java
@@ -0,0 +1,10 @@
+package cn.edu.hitsz.service.activity;
+
+import jakarta.servlet.annotation.WebServlet;
+
+/**
+ * @author SunDocker
+ */
+@WebServlet("/activity/showSignup")
+public class ActivityShowSignupService {
+}
